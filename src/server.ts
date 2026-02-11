a18765795e31c0b8fff8caaeb36be48529ef110c
Rahul Pal <usefulltech1819@gmail.com>
2026-02-11 19:27:46 +0530

    Add Mongo DB

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,17 @@
 import dotenv from "dotenv";
 import "dotenv/config"; 
+import { connectDb } from "./config/db.js";
 import app from "./app.js";
 
 dotenv.config();
-const PORT = process.env.PORT;
-app.listen(PORT, () => {
-    console.log("Server start at: ", PORT);
-});
+connectDb()
+    .then(() => {
+        const PORT = process.env.PORT;
+        app.listen(PORT, () => {
+            console.log("Server start at: ", PORT);
+        });
+    })
+    .catch((err) => {
+        console.error("MongoDB connection error: ", err);
+        process.exit(1);
+    });
