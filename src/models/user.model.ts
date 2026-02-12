7cf547dc1b431518afa36a58ac3bff12721a7373
Rahul Pal <usefulltech1819@gmail.com>
2026-02-12 18:25:30 +0530

    Add USER Schema

---
+++ src/models/user.model.ts
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+import type { USER } from "../types/index.js";
+
+const userSchma = new mongoose.Schema<USER>({
+    email: { type: String, required: true, unique: true },
+    name: { type: String, required: true, trim: true },
+    password: { type: String, required: true }
+});
+
+const UserModel = mongoose.models.USER as mongoose.Model<USER> || mongoose.model<USER>("USER", userSchma);
