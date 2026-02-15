2f28eb0d71657a8a26486db5821319ca58af43d1
Rahul Pal <usefulltech1819@gmail.com>
2026-02-15 22:04:15 +0530

    Update App.ts

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import { errorHandler } from "./middlewares/error.middleware.js";
+import AuthRoutes from "./routes/auth.route.js";
 
 const app = express();
 app.use(express.json({ limit: "16kb" }));
@@ -11,11 +12,6 @@ app.use(cors({
     allowedHeaders: ["Content-Type", "Authorization"]
 }));
 
-app.get('/', (req, res) => {
-    res.json({
-        msg: "Welcome!!"
-    })
-});
-
+app.use("/api/v1/auth", AuthRoutes);
 app.use(errorHandler);
 export default app;
