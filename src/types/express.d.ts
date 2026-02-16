6c7b06f7793482f3d8833b2f7c3f8e5e664a2fe3
Rahul Pal <usefulltech1819@gmail.com>
2026-02-16 21:18:16 +0530

    Add user type in Express Request

--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -1,10 +1,12 @@
 import type z from "zod";
 import type { validtionSchema } from "../middlewares/validation.middlewares.ts";
+import type { USER } from "./index.js";
 
 declare global {
     namespace Express {
         interface Request {
             validateData?: z.infer<typeof validtionSchema>;
+            user?: USER;
         }
     }
 }
