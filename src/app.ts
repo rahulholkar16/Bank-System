f56014eb90a0f7784a8cd8f4073e1fa37d5db82d
Rahul Pal <usefulltech1819@gmail.com>
2026-02-16 21:18:46 +0530

    Add cookie parser

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,12 @@ import express from "express";
 import cors from "cors";
 import { errorHandler } from "./middlewares/error.middleware.js";
 import AuthRoutes from "./routes/auth.route.js";
+import cookiesParser from "cookie-parser";
 
 const app = express();
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(cookiesParser());
 app.use(cors({
     origin: "*",
     credentials: true,
