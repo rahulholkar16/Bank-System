f2f095e147e21f7f4fd8efb963c98c2b3317a7d4
Rahul Pal <usefulltech1819@gmail.com>
2026-02-16 21:17:23 +0530

    Add Auth midleware

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,10 +1,18 @@
+import jwt, { type JwtPayload } from "jsonwebtoken";
 import { ApiError } from "../utils/ApiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { UserModel } from "../models/user.model.js";
 
 export const auth = asyncHandler(async (req, res, next) => {
     const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "");
     if (!token) throw new ApiError(401, "Unauthorized request");
     try {
+        const decode = jwt.verify(token, process.env.ACCESS_JWT_SECRET!) as JwtPayload;
+        const user = await UserModel.findById(decode?._id);
+        if (!user) throw new ApiError(401, "Invalid access token");
+        req.user = user;
+        next();
     } catch (error) {
+        throw new ApiError(401, "Invalid access token", error);
     }
 });
