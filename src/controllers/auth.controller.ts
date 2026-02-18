411d101660101607e0c715fb340171b2ba0622ce
Rahul Pal <usefulltech1819@gmail.com>
2026-02-18 13:09:14 +0530

    Add Login controller

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,8 +1,26 @@
-import type { Request, Response } from "express";
+import type { CookieOptions, Request, Response } from "express";
 import { UserModel } from "../models/user.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
+import type { Types } from "mongoose";
+
+const generateAccessAndRefreshToken = async (userId: string | Types.ObjectId) => {
+    try {
+        const user = await UserModel.findById(userId);
+        if (!user) throw new ApiError(404, "User not found");
+        const accessToken = user.generateAccessToken();
+        const refreshToken = user.generateRefreshToken();
+        user.refreshToken = refreshToken;
+        await user.save({ validateBeforeSave: false });
+        return { accessToken, refreshToken };
+    } catch (error) {
+        throw new ApiError(
+            500,
+            "Something went wrong while generating access token."
+        );
+    }
+};
 
 export const register = asyncHandler(async (req: Request, res: Response) => {
     const { email, password, name } = req.validateData;
@@ -29,3 +47,54 @@ export const register = asyncHandler(async (req: Request, res: Response) => {
         new ApiResponse(201, data, "User added successfully.")
     );
 });
+
+export const login = asyncHandler(async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+    if (!email || !password)
+        throw new ApiError(401, "Email and Password required.");
+
+    const user = await UserModel.findOne({ email });
+    if (!user) throw new ApiError(404, "User not found!");
+
+    const isPasswordValid = await user.isPasswordCorrect(password);
+    if (!isPasswordValid) throw new ApiError(401, "Invalid Password!");
+
+    const { accessToken, refreshToken } = await generateAccessAndRefreshToken(
+        user._id
+    );
+
+    const loggedUser = await UserModel.findById(user._id).select(
+        "-password -verificationToken -resetPasswordToken -refreshToken"
+    );
+
+    const accessCookieOptions: CookieOptions = {
+        httpOnly: true,
+        secure: false,
+        maxAge: 24 * 60 * 60 * 1000,
+        sameSite: "none"
+    };
+
+    const refreshCookieOptions: CookieOptions = {
+        httpOnly: true,
+        secure: false,
+        maxAge: 10 * 24 * 60 * 60 * 1000,
+        sameSite: "none"
+    };
+
+    return res
+        .status(200)
+        .cookie("accessToken", accessToken, accessCookieOptions)
+        .cookie("refreshToken", refreshToken, refreshCookieOptions)
+        .json(
+            new ApiResponse(
+                200,
+                {
+                    user: loggedUser,
+                    accessToken,
+                    refreshToken,
+                },
+                "User logged in successfully."
+            )
+        );
+});
+
